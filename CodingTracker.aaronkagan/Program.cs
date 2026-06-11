e888317cccdc2e69ca1bc1be02176fa4bee12134
Aaron Kagan <aaronkagan@gmail.com>
2026-06-10 22:14:18 -0400

    moving everything into one file during dev process

--- CodingTracker.aaronkagan/Program.cs
+++ CodingTracker.aaronkagan/Program.cs
@@ -1,5 +1,27 @@
-﻿using CodingTracker.aaronkagan;
+﻿using Spectre.Console;
+
 Menu.show();
+
+
+public class CodingSession
+{
+    private int Id { get; set; }
+    private TimeOnly StartTime { get; set; }
+    private TimeOnly EndTime { get; set; }
+    private DateOnly Date { get; set; }
+    
+}
+public class Menu
+{
+    internal static void show()
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Please choose an option?")
+                .AddChoices("View Coding Sessions", "Start Coding Session", "Add Coding Session", "Update Coding Session", "Delete Coding Session", "Exit Program"));
+    }
+}
+
 
 
 
